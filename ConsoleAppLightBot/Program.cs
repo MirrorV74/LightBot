e042b1e8d01949ffeb0b944a1902e4f1633ac04d
MirrorV74 <valera_sinitsa@bk.ru>
2023-02-01 00:39:32 +0300

    ReadMoves: added 2 types PlayMoves: base exceptions figured out

--- ConsoleAppLightBot/Program.cs
+++ ConsoleAppLightBot/Program.cs
@@ -1,15 +1,23 @@
-﻿char[,] FieldCreation()
+﻿using System.Diagnostics;
+
+char[,] FieldCreation()
 {
-    char[,] fieldLVL1 = new char[3, 3];
+    StreamReader reader = new StreamReader("1level.txt");
+
+    int cols = int.Parse(reader.ReadLine());
+    int rows = int.Parse(reader.ReadLine());
+
+    char[,] fieldLVL1 = new char[cols, rows];
     for (int i = 0; i < fieldLVL1.GetLength(0); i++)
     {
-        for (int j = 0; j < fieldLVL1.GetLength(1); j++)
+        char[] strfield = reader.ReadLine().ToCharArray();
+        for (int j = 0; j < strfield.Length; j++)
         {
-            fieldLVL1[i, j] = 'a';
+            fieldLVL1[i, j] = strfield[j];
         }
     }
-    fieldLVL1[0, 0] = 'X';
-    fieldLVL1[2, 2] = 'O';
+
+    reader.Close();
 
     return fieldLVL1;
 }
@@ -20,11 +28,141 @@ void PrintField(char[,] field)
     {
         for (int j = 0; j < field.GetLength(1); j++)
         {
-            Console.Write(field[i,j]);
+            Console.Write(field[i, j]);
         }
         Console.WriteLine();
     }
 }
-char[,] field = FieldCreation();
 
-PrintField(field);
+char[] ReadMoves() 
+{
+    Console.WriteLine("Choose type of imput:");
+    Console.WriteLine("1. From file");
+    Console.WriteLine("2. By hands");
+    int type = int.Parse(Console.ReadLine());
+    char[] moves;
+
+    switch (type)
+    {
+        case 1:
+        {
+            Console.WriteLine("Write file name");
+            string file = Console.ReadLine();
+            
+            StreamReader reader = new StreamReader($"{file}");
+            moves = reader.ReadLine().ToCharArray();
+            reader.Close();
+            
+            break;
+        }
+        case 2:
+        {
+            Console.WriteLine("Write moves");
+            moves = Console.ReadLine().ToCharArray();
+            break;
+        }
+        default:
+        {
+            moves = new char[1];
+            break;
+        }
+    }
+    
+    return moves;
+}
+
+int[] FindPlayer(char[,] field)
+{
+    int[] playerCoordinates = new int[2];
+    for (int i = 0; i < field.GetLength(0); i++)
+    {
+        for (int j = 0; j < field.GetLength(1); j++)
+        {
+            if (field[i,j] == 'X')
+            {
+                playerCoordinates[0] = i;
+                playerCoordinates[1] = j;
+            }
+        }
+    }
+    return playerCoordinates;
+}
+
+void PlayMoves(char[] moves, char[,] field)
+{
+    int[] playerCoordinates = FindPlayer(field);
+    int playerX = playerCoordinates[0];
+    int playerY = playerCoordinates[1];
+    field[playerX, playerY] = 'a';
+    bool allowedCombination = true;
+    for (int i = 0; i < moves.Length && allowedCombination; i++)
+    {
+        switch (moves[i])
+        {
+            case 'w':
+            {
+                if (playerY != 0)
+                {
+                    playerY--;
+                }
+                else
+                {
+                    allowedCombination = false;
+                    Console.WriteLine("Impossible movement");
+                }
+                break;
+            }
+            case 'a':
+            {
+                if (playerX != 0)
+                {
+                    playerX--;
+                }
+                else
+                {
+                    allowedCombination = false;
+                    Console.WriteLine("Impossible movement");
+                }
+                break;
+            }
+            case 's':
+            {
+                if (playerY != field.GetLength(1))
+                {
+                    playerY++;
+                }
+                else
+                {
+                    allowedCombination = false;
+                    Console.WriteLine("Impossible movement");
+                }
+                break;
+            }
+            case 'd':
+            {
+                if (playerX != field.GetLength(0))
+                {
+                    playerX++;
+                }
+                else
+                {
+                    allowedCombination = false;
+                    Console.WriteLine("Impossible movement");
+                }
+                break;
+            }
+            default:
+            {
+                allowedCombination = false;
+                Console.WriteLine("Incorrect input");
+                break;
+            }
+        }
+        field[playerX, playerY] = 'X';
+        PrintField(field);
+    }
+}
+
+char[,] field = FieldCreation();
+char[] moves = ReadMoves();
+PlayMoves(moves, field);
