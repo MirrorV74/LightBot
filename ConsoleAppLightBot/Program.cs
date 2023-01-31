7b9facf5095cddec95a508a802d6db666a6c77ed
MirrorV74 <valera_sinitsa@bk.ru>
2023-01-31 17:43:11 +0300

    field creation

--- ConsoleAppLightBot/Program.cs
+++ ConsoleAppLightBot/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿char[,] FieldCreation()
+{
+    char[,] fieldLVL1 = new char[3, 3];
+    for (int i = 0; i < fieldLVL1.GetLength(0); i++)
+    {
+        for (int j = 0; j < fieldLVL1.GetLength(1); j++)
+        {
+            fieldLVL1[i, j] = 'a';
+        }
+    }
+    fieldLVL1[0, 0] = 'X';
+    fieldLVL1[2, 2] = 'O';
 
-Console.WriteLine("Hello, World!");
+    return fieldLVL1;
+}
+
+void PrintField(char[,] field)
+{
+    for (int i = 0; i < field.GetLength(0); i++)
+    {
+        for (int j = 0; j < field.GetLength(1); j++)
+        {
+            Console.Write(field[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+char[,] field = FieldCreation();
+
+PrintField(field);
